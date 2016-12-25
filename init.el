f4b034f9bd126f117516863952a6fbd9a6a1336e
shiki0711 <emit.yanfeng@gmail.com>
2016-12-25 15:54:10 +0800

    Disabled un-supported ipython features

--- init.el
+++ init.el
@@ -5,11 +5,18 @@
 
 (require 'package)
 
-(add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;        '("melpa" . "http://melpa.org/packages/") t)
   
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
-			 ("gnu" . "http://elpa.gnu.org/packages/")))
+;; (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+;; 			 ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(setq package-archives  
+       '(("gnu"          . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/") 
+         ("melpa"        . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") 
+         ("melpa-stable" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/") 
+         ("org"          . "https://mirrors.tuna.tsinghua.edu.cn/elpa/org/") 
+         ("marmalade"    . "https://mirrors.tuna.tsinghua.edu.cn/elpa/marmalade/")))
 
 (package-initialize)
 
@@ -50,6 +57,13 @@
 ;; enable autopep8 formatting on save
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;;; Fix junk characters in shell-mode  
+;;; (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t) 
+;;; (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on t)
+
+;; Not supported IPython 5's new prompt behavior.
+(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
 
 ;; init.el ends here
 
