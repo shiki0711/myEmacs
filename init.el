2b3eec13482a9e14d1140617775b7046c0be6d99
shiki0711 <emit.yanfeng@gmail.com>
2016-12-25 16:13:30 +0800

    Disable ipython new features

--- init.el
+++ init.el
@@ -58,5 +58,22 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
+;; Not surpported IPython 5's new prompt behavior.
+(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
+
 ;; init.el ends here
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (py-autopep8 material-theme flycheck elpy ein better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
